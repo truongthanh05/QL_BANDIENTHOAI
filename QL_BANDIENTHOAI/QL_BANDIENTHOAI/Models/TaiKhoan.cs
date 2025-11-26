a7b10c969b503ae3019a1ec8fb99c39ab3fe46e4
truongthanh05 <truongthanh582563@gmail.com>
2025-11-26 14:53:50 +0700

    update account

--- QL_BANDIENTHOAI/QL_BANDIENTHOAI/Models/TaiKhoan.cs
+++ QL_BANDIENTHOAI/QL_BANDIENTHOAI/Models/TaiKhoan.cs
@@ -7,12 +7,15 @@ namespace QL_BANDIENTHOAI.Models
 {
     public class TaiKhoan
     {
-        public string Id { get; set; }
-        public string Matk { get; set; }
-        public string TenTk { get; set; }
-        public string MatKhau { get; set; }
-
-        // Navs
+        public string MATK { get; set; }     // Mã tài khoản
+        public string ID { get; set; }       // Mã người dùng
+        public string TENTK { get; set; }    // Tên tài khoản
+        public string MATKHAU { get; set; }  // Mật khẩu
+        public string ROLE { get; set; }     // Vai trò
+        public string TRANGTHAI { get; set; } // Trạng thái tài khoản
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }    // Email người dùng
+        public string PhoneNumber { get; set; }
         public NguoiDung NguoiDung { get; set; }
         public ICollection<GioHang> GioHangs { get; set; }
 
